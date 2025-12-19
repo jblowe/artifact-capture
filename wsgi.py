13ff21361d98fa14adcd19001284d1791e25c48c
John B. Lowe <jblowe@berkeley.edu>
2025-12-18 23:46:44 -0800

    remove authentication defaults from wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,1 @@
-import os
 from app import app as application
-
-os.environ.setdefault("ARTCAP_ADMIN_USER", "admin")
-os.environ.setdefault("ARTCAP_ADMIN_PASS", "change-me")
-os.environ.setdefault("ARTCAP_SECRET", "change-me-secret")
